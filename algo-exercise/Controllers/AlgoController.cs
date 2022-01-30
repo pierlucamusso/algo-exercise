e15d906c81d90332bf5512523dc3672811c5bdfb
Pierluca Musso <pierlucamusso@MacBook-Pro-di-Pierluca.local>
2022-01-30 10:50:08 +0100

    Creating Solution

---
+++ algo-exercise/Controllers/AlgoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace algo_exercise.Controllers
+{
+    public class AlgoController : Controller
+    {
+        private readonly ILogger<AlgoController> _logger;
+
+        public AlgoController(ILogger<AlgoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
