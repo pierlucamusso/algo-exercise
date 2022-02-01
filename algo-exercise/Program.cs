951f72c292eebe9109b535f05f2877700adf0744
Pierluca Musso <pierlucamusso@MacBook-Pro-di-Pierluca.local>
2022-02-01 08:44:34 +0100

    change sln

--- algo-exercise/Program.cs
+++ algo-exercise/Program.cs
@@ -8,8 +8,20 @@ namespace algo_exercise
         {
             Console.WriteLine("Exercise 1");
             int res = Exercise1(1000);
-            Console.WriteLine("Result for 1000 input" + res);
+            Console.WriteLine("Result for 1000 input " + res);
+
+            Console.WriteLine("Exercise 2");
+            res = Exercise2(4000000);
+            Console.WriteLine("Result for 4000000 limit " + res);
+
+            Console.WriteLine("Exercise 3");
+            res = Exercise2(13195);
+            Console.WriteLine("Result for 4000000 limit " + res);
         }
+
+        /**
+         https://projecteuler.net/archives
+         */
 
         public static int Exercise1(int limit)
         {
@@ -23,5 +35,57 @@ namespace algo_exercise
 
             return result;
         }
+
+        public static int Exercise2(int limit)
+        {
+            int pointer = 1;
+            int lastSequenceNumber = 1;
+            int evenSum = 0;
+
+            while(lastSequenceNumber <= limit)
+            {
+                var tmp = lastSequenceNumber;
+                lastSequenceNumber += pointer;
+                pointer = tmp;
+
+                if (lastSequenceNumber > limit)
+                    break;
+
+                if (lastSequenceNumber % 2 == 0)
+                    evenSum += lastSequenceNumber;
+            }
+
+            return evenSum;
+        }
+
+        public static int Exercise3(int number)
+        {
+            int currentPrimeFactor = 2;
+
+            while (!IsPrime(currentPrimeFactor))
+            {
+                if (number % currentPrimeFactor == 0)
+                    number /= currentPrimeFactor;
+            }
+
+            return currentPrimeFactor;
+        }
+
+        public static bool IsPrime(int number)
+        {
+            int dividers = 0;
+            int divisor = 1;
+
+            while (dividers < 3 || divisor >= number / 2)
+            {
+                if (number % divisor == 0)
+                    dividers++;
+
+                divisor++;
+            }
+
+            return dividers <= 2;
+        }
+
     }
 }
